701c73016b84a3d5bdf7f73eee3cd818ffd4b876
markshlyakovsky <78624548+markshlyakovsky@users.noreply.github.com>
2023-05-21 00:31:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function searchFlights() {
+  window.location.href = "flights.html";
+}
+
+function searchHotels() {
+  window.location.href = "flights.html";
+}
+
+function searchCombos() {
+  window.location.href = "flights.html";
+}
